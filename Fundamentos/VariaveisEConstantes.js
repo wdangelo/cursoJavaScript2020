488c2efa76e81012788f085cb0616ee24ae5e79d
William de Angelo <52357299+wdangelo@users.noreply.github.com>
2020-09-21 16:47:08 -0300

    inicio do curso

---
+++ Fundamentos/VariaveisEConstantes.js
@@ -0,0 +1,19 @@
+var a = 3
+
+let b = 4
+
+var a = 30
+b = 40
+
+
+console.log(a, b)
+
+a = 300
+
+a = 400
+
+console.log(a, b)
+
+const c = 30
+
+console.log(c)
---
+++ Fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log('Sentença de código')
+
+{
+     {
+         console.log("ola")
+         console.log('Mundo!')
+     }
+}
+
+//JavaScript é identado por Sentaças de códigos e blocos de códigos que são delimitados por chaves
